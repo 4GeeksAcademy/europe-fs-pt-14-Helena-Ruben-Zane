f939a0e30f432fc5c8036c1cd3e045c2d5849085
helenaraimundo91 <139161478+helenaraimundo91@users.noreply.github.com>
2024-02-05 20:27:32 +0000

    see the cleaners button and who to put the forms in the middle of the image

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../../img/logo.png";
+
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+		<nav className="nav">
+			<div className="container nav__menu">
+				<img src={logo} className="nav__logo"/>
+				<Link to="/home" className="nav__btn round">
+					<span>CLEANERS</span>
 				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
 			</div>
 		</nav>
 	);
 };
+
+
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,19 +1,27 @@
 import React, { useContext } from "react";
 import "../../styles/home.css";
 import { Link } from "react-router-dom";
+import { Navbar } from "../component/navbar";
+import Image5 from "../../img/image5.jpg";
 
 export const Home = () => {
-
-
 	return (
-		<div className="text-center mt-5">
-			<h1>Home Page</h1>
-			<Link to="/signup">
-					<button className="btn btn-primary">Signup</button>
-			</Link>
-			<Link to="/login">
-					<button className="btn btn-primary">Login</button>
-			</Link>
+		<div className="home">
+			<Navbar />
+			<div className="hero">
+				<img className="hero__image" src={Image5} />
+			</div>
+			<div className="container box">
+				<div className="text-center mt-5">
+					<Link to="/signup">
+						<button><strong>SIGN UP</strong></button>
+					</Link>
+					<Link to="/login">
+						<button><strong>LOGIN</strong></button>
+					</Link>
+				</div>
+			</div>
 		</div>
+
 	);
 };
